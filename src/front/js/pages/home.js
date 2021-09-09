bc2404e03677ccc0c589ebcf2544d06a9b1755e2
GMDEEP <stewartjgreen@gmail.com>
2021-09-09 23:56:39 +0000

    updated home page

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,43 +7,45 @@ import Carousel from "react-bootstrap/Carousel";
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
-	<Carousel fade>
-		<Carousel.Item>
-			<img
-				className="d-block carouselImg"
-				src="https://res.cloudinary.com/djwr65ikt/image/upload/v1630434385/368503_oz3s3i.jpg"
-				alt="First slide"
-			/>
-			<Link to="/markets">
-				<Carousel.Caption>
-					<h5>Top 10 CryptoCoins</h5>
-				</Carousel.Caption>
-			</Link>
-		</Carousel.Item>
-		<Carousel.Item>
-			<img
-				className="d-block carouselImg"
-				src="https://res.cloudinary.com/djwr65ikt/image/upload/v1630434753/star-wars-star-wars-the-fallen-order-planet-digital-art-artwork-hd-wallpaper-preview_poejdb.jpg"
-				alt="Second slide"
-			/>
-			<Link to="/videos">
-				<Carousel.Caption>
-					<h5>Crypto Video Library</h5>
-					<p>Learn how to trade crypto or learn about what crypto is in our video library</p>
-				</Carousel.Caption>
-			</Link>
-		</Carousel.Item>
-		<Carousel.Item>
-			<img
-				className="d-block carouselImg"
-				src="https://res.cloudinary.com/djwr65ikt/image/upload/v1630434944/ea1097a6ee800831b32c0e94ab9e6818_hywrnq.jpg"
-				alt="Third slide"
-			/>
-			<Link to="/research">
-				<Carousel.Caption>
-					<h5>Research the Market Activity</h5>
-				</Carousel.Caption>
-			</Link>
-		</Carousel.Item>
-	</Carousel>;
+	return (
+		<Carousel fade>
+			<Carousel.Item>
+				<img
+					className="d-block carouselImg"
+					src="https://res.cloudinary.com/djwr65ikt/image/upload/v1631231393/download_1_wbm3fn.jpg"
+					alt="First slide"
+				/>
+				<Link to="/coinpage/:coin">
+					<Carousel.Caption>
+						<h5>Top 10 CryptoCoins</h5>
+					</Carousel.Caption>
+				</Link>
+			</Carousel.Item>
+			<Carousel.Item>
+				<img
+					className="d-block carouselImg"
+					src="https://res.cloudinary.com/djwr65ikt/image/upload/v1631231402/image_nfut09.jpg"
+					alt="Second slide"
+				/>
+				<Link to="/videos">
+					<Carousel.Caption>
+						<h5>Crypto Video Library</h5>
+						<p>Learn how to trade crypto or learn about what crypto is in our video library</p>
+					</Carousel.Caption>
+				</Link>
+			</Carousel.Item>
+			<Carousel.Item>
+				<img
+					className="d-block carouselImg"
+					src="https://res.cloudinary.com/djwr65ikt/image/upload/v1631231411/cryptoLP_banner_3_jpg_hdg5tf.jpg"
+					alt="Third slide"
+				/>
+				<Link to="/research">
+					<Carousel.Caption>
+						<h5>Research the Market Activity</h5>
+					</Carousel.Caption>
+				</Link>
+			</Carousel.Item>
+		</Carousel>
+	);
 };
