d5d6dca6279d796be0552f7ef83781969707b419
GMDEEP <stewartjgreen@gmail.com>
2021-09-10 20:35:21 +0000

    updated chart on content page

--- src/front/js/pages/ContentPage.js
+++ src/front/js/pages/ContentPage.js
@@ -2,6 +2,12 @@ import React, { useEffect, useState } from "react";
 
 export function ContentPage() {
 	const [market, setMarket] = useState([]);
+	var React = require('react');
+	var Component = React.Component;
+	var CanvasJSReact = require('./canvasjs.react');
+	var CanvasJS = CanvasJSReact.CanvasJS;
+	var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+	var dataPoints = [];
 
 	// useEffect(() => {
 	// 	fetch("#/markets")
@@ -13,5 +19,56 @@ export function ContentPage() {
 	// 	})
 	// }, []);
 
-	return <div className="container" />;
-}
+	return (
+		/* App.js */
+	class App extends Component {
+		render() {
+			const options = {
+				exportEnabled: true,
+				title: {
+					text: "Microsoft Corporation Stock Price - December 2017"
+				},
+				axisX: {
+					valueFormatString: "D MMM"
+				},
+				axisY: {
+					title: "Price",
+					prefix: "$"
+				},
+				data: [{
+					type: "candlestick",
+					name: "Microsoft Corporation Price",
+					showInLegend: true,
+					yValueFormatString: "$##0.00",
+					xValueType: "dateTime",
+					dataPoints: dataPoints
+				}]
+			}
+			return (
+				<div>
+					<CanvasJSChart options={options}
+						onRef={ref => this.chart = ref}
+					/>
+					{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+				</div>
+			);
+		}
+		componentDidMount() {
+			var chart = this.chart;
+			fetch('https://canvasjs.com/data/gallery/react/microsoft-stock-price.json')
+				.then(function (response) {
+					return response.json();
+				})
+				.then(function (data) {
+					for (var i = 0; i < data.length; i++) {
+						dataPoints.push({
+							x: data[i].x,
+							y: data[i].y
+						});
+					}
+					chart.render();
+				});
+		}
+	}
+	module.exports = App;                              
+	)
