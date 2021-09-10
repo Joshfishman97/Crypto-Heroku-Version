2d62b03f8b8703356808c64905e0f7940b67ad0f
GMDEEP <stewartjgreen@gmail.com>
2021-09-10 01:22:07 +0000

    updated coinpage and fixed the error from adding table

--- src/front/js/pages/CoinPage.js
+++ src/front/js/pages/CoinPage.js
@@ -1,66 +1,42 @@
 import React, { useEffect, useState } from "react";
 
 export function CoinPage() {
-    const [coin, setCoin] = useState([]);
+	const [coins, setCoins] = useState([]);
 
-    useEffect(() => {
-        fetch(process.env.BACKEND_URL + "/api/coin")
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setCoin(data);
-            });
-    }, []);
+	useEffect(() => {
+		fetch(process.env.BACKEND_URL + "/api/coin")
+			.then(res => {
+				return res.json();
+			})
+			.then(data => {
+				setCoins(data);
+			});
+	}, []);
 
-    return (
-        <div className="d-flex flex-column">
-            {coin ? coin.map((coin, i) => {
-                return (
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Coin</th>
-                                <th scope="col">Price</th>
-                                <th scope="col">Daily Gain/Loss</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">{coin.id}</th>
-                                <td>{coin.namecoin}</td>
-                                <td>{coin.price}</td>
-                                <td>{coin.usd_24th_change}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">{coin.id}</th>
-                                <td>{coin.namecoin}</td>
-                                <td>{coin.price}</td>
-                                <td>{coin.usd_24th_change}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">{coin.id}</th>
-                                <td>{coin.namecoin}</td>
-                                <td>{coin.price}</td>
-                                <td>{coin.usd_24th_change}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">{coin.id}</th>
-                                <td>{coin.namecoin}</td>
-                                <td>{coin.price}</td>
-                                <td>{coin.usd_24th_change}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">{coin.id}</th>
-                                <td>{coin.namecoin}</td>
-                                <td>{coin.price}</td>
-                                <td>{coin.usd_24th_change}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                )
-            }) 
-        </div>
-    );
+	return (
+		<div className="container">
+			<table className="table">
+				<thead>
+					<tr>
+						<th scope="col">#</th>
+						<th scope="col">Coin</th>
+						<th scope="col">Price</th>
+						<th scope="col">Daily Gain/Loss</th>
+					</tr>
+				</thead>
+				<tbody>
+					{coins.map((coin, i) => {
+						return (
+							<tr>
+								<th scope="row">{coin.id}</th>
+								<td>{coin.namecoin}</td>
+								<td>{coin.price}</td>
+								<td>{coin.usd_24th_change}</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</div>
+	);
 }
