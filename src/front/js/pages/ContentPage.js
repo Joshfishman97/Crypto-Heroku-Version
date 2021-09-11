d55c9a244b1281ebfc97239dacfee0dfeef0dfee
GMDEEP <stewartjgreen@gmail.com>
2021-09-11 16:20:52 +0000

    added chart

--- src/front/js/pages/ContentPage.js
+++ src/front/js/pages/ContentPage.js
@@ -1,14 +1,7 @@
 import React, { useEffect, useState } from "react";
+import Chart from "react-google-charts";
 
 export function ContentPage() {
-	const [market, setMarket] = useState([]);
-	var React = require('react');
-	var Component = React.Component;
-	var CanvasJSReact = require('./canvasjs.react');
-	var CanvasJS = CanvasJSReact.CanvasJS;
-	var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-	var dataPoints = [];
-
 	// useEffect(() => {
 	// 	fetch("#/markets")
 	// 	.then(res => {
@@ -20,55 +13,28 @@ export function ContentPage() {
 	// }, []);
 
 	return (
-		/* App.js */
-	class App extends Component {
-		render() {
-			const options = {
-				exportEnabled: true,
-				title: {
-					text: "Microsoft Corporation Stock Price - December 2017"
-				},
-				axisX: {
-					valueFormatString: "D MMM"
-				},
-				axisY: {
-					title: "Price",
-					prefix: "$"
-				},
-				data: [{
-					type: "candlestick",
-					name: "Microsoft Corporation Price",
-					showInLegend: true,
-					yValueFormatString: "$##0.00",
-					xValueType: "dateTime",
-					dataPoints: dataPoints
-				}]
-			}
-			return (
-				<div>
-					<CanvasJSChart options={options}
-						onRef={ref => this.chart = ref}
-					/>
-					{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-				</div>
-			);
-		}
-		componentDidMount() {
-			var chart = this.chart;
-			fetch('https://canvasjs.com/data/gallery/react/microsoft-stock-price.json')
-				.then(function (response) {
-					return response.json();
-				})
-				.then(function (data) {
-					for (var i = 0; i < data.length; i++) {
-						dataPoints.push({
-							x: data[i].x,
-							y: data[i].y
-						});
-					}
-					chart.render();
-				});
-		}
-	}
-	module.exports = App;                              
-	)
+		<Chart
+			width={"100%"}
+			height={350}
+			chartType="CandlestickChart"
+			loader={<div>Loading Chart</div>}
+			data={[
+				["day", "a", "b", "c", "d"],
+				["Mon", 20, 28, 38, 45],
+				["Tue", 31, 38, 55, 66],
+				["Wed", 50, 55, 77, 80],
+				["Thu", 77, 77, 66, 50],
+				["Fri", 68, 66, 22, 15]
+			]}
+			options={{
+				legend: "none",
+				bar: { groupWidth: "100%" }, // Remove space between bars.
+				candlestick: {
+					fallingColor: { strokeWidth: 0, fill: "#a52714" }, // red
+					risingColor: { strokeWidth: 0, fill: "#0f9d58" } // green
+				}
+			}}
+			rootProps={{ "data-testid": "2" }}
+		/>
+	);
+}
