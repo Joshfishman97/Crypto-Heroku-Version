1b582c723b968fff11c8e23abb664b9528a47a73
GMDEEP <stewartjgreen@gmail.com>
2021-09-17 00:51:57 +0000

    updated video page

--- src/front/js/pages/VideoPage.js
+++ src/front/js/pages/VideoPage.js
@@ -13,5 +13,43 @@ export function VideoPage() {
 	// 	})
 	// }, []);
 
-	return <div className="container" />;
+	return (
+		<div className="container">
+			<div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+				<div className="ratio ratio-16x9">
+					<iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" />
+				</div>
+				<div className="ratio ratio-16x9">
+					<iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" />
+				</div>
+				<div className="ratio ratio-16x9">
+					<iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" />
+				</div>
+				<div className="ratio ratio-16x9">
+					<iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" />
+				</div>
+				<div className="ratio ratio-16x9">
+					<iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" />
+				</div>
+				<div className="ratio ratio-16x9">
+					<iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" />
+				</div>
+				<div className="ratio ratio-16x9">
+					<iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" />
+				</div>
+				<div className="ratio ratio-16x9">
+					<iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" />
+				</div>
+				<div className="ratio ratio-16x9">
+					<iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" />
+				</div>
+				<div className="ratio ratio-16x9">
+					<iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" />
+				</div>
+				<div className="ratio ratio-16x9">
+					<iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" />
+				</div>
+			</div>
+		</div>
+	);
 }
