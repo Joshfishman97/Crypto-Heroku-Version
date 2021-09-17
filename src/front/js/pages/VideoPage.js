e73ba7a3ce091455b16422326cab4a7446aab301
GMDEEP <stewartjgreen@gmail.com>
2021-09-17 00:55:32 +0000

    updated video page

--- src/front/js/pages/VideoPage.js
+++ src/front/js/pages/VideoPage.js
@@ -49,6 +49,9 @@ export function VideoPage() {
 				<div className="ratio ratio-16x9">
 					<iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" />
 				</div>
+				<div className="ratio ratio-16x9">
+					<iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" />
+				</div>
 			</div>
 		</div>
 	);
